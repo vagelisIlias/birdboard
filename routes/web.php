cbde0105cea663d19722062f9940f2b3e22f0d01
evangelosIlias <evangelos.ilias87@gmail.com>
2024-01-25 21:40:53 +0000

    Initial Deploy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/projects', function() {
+
+    Project::create(request(['title', 'description']));
+
 });
